cc35321a0602f56e7b4f4bd315917cf91fb36501
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2022-11-27 22:16:40 -0500

    Set only Build and Tag pipelines for now (#29)

--- deploy/google-cloud/terraform/bootstrap/modules/artifact_registry/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "backend_image" {
-  value       = local.backend_image
-  description = "The Backend Docker image."
-}
--- deploy/google-cloud/terraform/bootstrap/modules/artifact_registry/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  type        = string
-  description = "The project ID. Changing this forces a new project to be created."
-}
-
-variable "region" {
-  type        = string
-  description = "The default region to manage resources in. If another region is specified on a regional resource, it will take precedence."
-}
